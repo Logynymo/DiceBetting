51b8f7292fbdd0ba3ef69bbed088f441d921ed96
Logynymo <mathiasry1@gmail.com>
2023-08-10 00:29:23 +0200

    chatgpt

--- DiceBetting.py
+++ DiceBetting.py
@@ -1,12 +1,11 @@
-
 import random
-import math
 
-newGame = False
-
-def main(newGame):
-    while newGame == False():
+def newGame():
+    while True:
         bet = input("What number will the dice roll?\nYou can also bet higher or lower by using < or > (lower than x or higher than x) ")
         wager = input("What amount of money would you like to wager? ")
         wager = int(wager)
-        
+
+        diceRoll = random.randint(1, 6)
+
+        print(f"The dice rolled: {diceRoll}")
